9dae8c6d375223f3dd9efdc96b8a60324a76e0da
Cristian Sombra <cristiansombra87@gmail.com>
2024-08-29 23:46:57 -0300

    Proyect create

---
+++ gestordeordenes/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Gestor de órdenes Next.js con App router prisma",
+  description: "Gestor de órdenes Next.js con App router prisma",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
